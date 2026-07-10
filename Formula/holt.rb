8a14e5017af8028543db51014f5f2740c76351b3
Debashis Nayak <debashis.deb94@gmail.com>
2026-07-10 14:41:03 +0000

    holt 0.14.2

--- Formula/holt.rb
+++ Formula/holt.rb
@@ -1,8 +1,8 @@
 class Holt < Formula
   desc "Open-source personal agent OS: any LLM, private memory you can see"
   homepage "https://productsdecoded.com/holt"
-  url "https://registry.npmjs.org/@holt-os/holt/-/holt-0.14.1.tgz"
-  sha256 "fcececd109fa83a8cfd6c28e09507516c7908e8c03a2e22090c32fae8a9f3eb8"
+  url "https://registry.npmjs.org/@holt-os/holt/-/holt-0.14.2.tgz"
+  sha256 "ee81200c67cee4da1d13e48899597e771a7cfddb106742eb727277bfe8ba2063"
   license "MIT"
 
   depends_on "node"
